6b070ad4d2d24fe980964c2ec49f86fcc5d4a43c
Andy <andy_wang347@hotmail.com>
2017-01-21 02:05:32 -0800

    Script for shrinking play field

---
+++ Assets/Scripts/PlayFieldScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayFieldScript : MonoBehaviour {
+
+    private float shrinkSpeed = 0.002f;
+    private int minScale = 1; 
+	
+	void Update () {
+        if(transform.localScale.x >= minScale) {
+            transform.localScale -= new Vector3(shrinkSpeed, 0, shrinkSpeed);
+        }
+    }
+}
