54ec37f83154751e32d88925e630343fa8f4b907
Andy <andy_wang347@hotmail.com>
2017-01-21 13:26:26 -0800

    Move player up on death floor

---
+++ Assets/Scripts/DeathFloor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathFloor : MonoBehaviour {
+
+    public Camera playerCamera;
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+   
+        if (other.gameObject.CompareTag("Death_floor"))
+        {
+            gameObject.transform.position = new Vector3(0, 63, 2);
+            gameObject.transform.rotation = new Quaternion(90, 0, 0, 0);
+            Debug.Log("Touching death floor"); 
+        }
+    }
+
+
+
+
+}
