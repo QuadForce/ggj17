135b1318a856f44adf688ed38669c66ac36eb1ad
Nbalsh <brandon.bn.nelson@gmail.com>
2017-01-22 13:24:22 -0800

    Game Level

---
+++ Assets/Scripts/PathDefinition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathDefinition : MonoBehaviour {
+
+public Transform[] Points;
+    private bool loopPath = true;
+
+	public IEnumerator<Transform> GetPathEnumerator(){
+		if (Points == null || Points.Length < 1)
+			yield break;
+
+		var direction = 1;
+		var index = 0;
+		while (loopPath) {
+            yield return Points[index];
+			if(Points.Length == 1)
+				continue;
+            if (index <= 0)
+                direction = 1;
+            else if (index >= Points.Length - 1)
+            {
+				direction = -1;
+            }
+			index = index + direction;
+
+		}
+	}
+}
