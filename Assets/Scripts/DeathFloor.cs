f5fa8f6c2c77e7cedab8ba7c3274c72d3962d21a
Michael Kwan <mike.kwan@hotmail.com>
2017-01-22 09:30:27 -0800

    death particle effect

--- Assets/Scripts/DeathFloor.cs
+++ Assets/Scripts/DeathFloor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DeathFloor : MonoBehaviour {
 
-    public Camera playerCamera; 
+    public GameObject particles;
     private float spawnChoice; 
 
     void Start()
@@ -17,7 +17,10 @@ public class DeathFloor : MonoBehaviour {
    
         if (other.gameObject.CompareTag("Death_floor"))
         {
+            Vector3 particleLocation = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1.5f, gameObject.transform.position.z);
+            GameObject pEffect = GameObject.Instantiate(particles, particleLocation, new Quaternion(0,0,0,0));
             gameObject.transform.position = new Vector3(11.86f, 60f, 22.0f);
+            Destroy(pEffect, 1.8f);
         }
     }
 
