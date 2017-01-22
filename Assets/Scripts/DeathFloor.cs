33482d7cd5bb97b91e4a93d23aa1f00d6b71c3bb
Andy <andy_wang347@hotmail.com>
2017-01-22 01:50:29 -0800

    Match end view

--- Assets/Scripts/DeathFloor.cs
+++ Assets/Scripts/DeathFloor.cs
@@ -17,23 +17,7 @@ public class DeathFloor : MonoBehaviour {
    
         if (other.gameObject.CompareTag("Death_floor"))
         {
-            spawnChoice = Random.Range(1, 5);
-            if (spawnChoice == 1)
-            {
-                gameObject.transform.position = new Vector3(61.2f, 10f, 67.9f);
-            }
-            if (spawnChoice == 2)
-            {
-                gameObject.transform.position = new Vector3(-33.4f, 10f, 65.0f);
-            }
-            if (spawnChoice == 3)
-            {
-                gameObject.transform.position = new Vector3(-21.8f, 10f, -21.8f);
-            }
-            if (spawnChoice == 4)
-            {
-                gameObject.transform.position = new Vector3(56.1f, 10f, -23.6f);
-            }
+            gameObject.transform.position = new Vector3(11.86f, 60f, 22.0f);
         }
     }
 
