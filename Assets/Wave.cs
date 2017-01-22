bcf477f72a70c1bb5a4a76be901a4741644dbe8a
Andy Chan <andychan94@live.com>
2017-01-22 01:34:36 -0800

    waving script created and working for hand bullets

---
+++ Assets/Wave.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+    bool right = true;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        var rotationVector = transform.rotation.eulerAngles;
+
+        if (right)
+        {
+            rotationVector.z += 2;
+            
+            transform.rotation = Quaternion.Euler(rotationVector);
+            if (rotationVector.z > 110)
+            {
+                right = false;
+            }
+        }
+
+        if (!right)
+        {
+            rotationVector.z -= 2;
+            transform.rotation = Quaternion.Euler(rotationVector);
+            if (rotationVector.z < 65)
+            {
+                right = true;
+            }
+        }
+
+    }
+}
+    
